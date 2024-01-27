d8b52f935d140aae20afff405a8019bc126c1dc3
Kyle Piazza-Nickson <zlac156@live.rhul.ac.uk>
2024-01-27 18:07:17 +0000

    Changes AttributeData to use generics

--- src/main/java/application/AttributeData.java
+++ src/main/java/application/AttributeData.java
@@ -5,21 +5,28 @@ package application;
  * Container class for all types of data that can be stored in the attribute dictionary for any given asset.
  * Allows for a variety of types of data to be stored in a single data structure.
  */
-public class AttributeData {
+public class AttributeData <T> {
 	
 	private DataType type;
 	
-	public AttributeData(String data) {
-		this.type = DataType.STRING;
+	private T value;
+	
+	public AttributeData(T data) {
+		this.value = data;
+		if (value instanceof String) {
+			this.type = DataType.STRING;
+		} else {
+			this.type = DataType.FLOAT;
+		}
 	}
 	
 	public DataType getType() {
 		return type;
 	}
 	
-	public AttributeData(float data) {
-		this.type = DataType.FLOAT;
+	//public AttributeData(float data) {
+		//this.type = DataType.FLOAT;
 		
-	}
+	//}
 
 }
