bbbe7b21c6a7c080f98f59ab2ec22306ac8ce049
HIEN_PHAN <wkis088@rhul.live.ac.uk>
2024-02-19 21:20:30 +0000

    add Pagination to table,add Delete and Edit Button

--- UI/src/components/ViewAssets.js
+++ UI/src/components/ViewAssets.js
@@ -1,24 +1,39 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Title from './Title';
-import * as AssetAPI from '../AssetAPI';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Title from "./Title";
+import { Link } from "react-router-dom";
+import * as AssetAPI from "../AssetAPI";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import styles from "../style/listItems.module.css";
+import { IconButton, TablePagination } from "@mui/material";
 
 export default function ViewAssets() {
+  const [assets, setAssets] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
   React.useEffect(() => {
     const getAssets = async () => {
       const res = await AssetAPI.getAll();
-      console.log(res)
-      setAssets(res)
+      setAssets(res);
     };
 
     getAssets();
   }, []);
 
-  const [assets, setAssets] = React.useState([])
+  const handleChangePage = (newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <React.Fragment>
@@ -31,20 +46,43 @@ export default function ViewAssets() {
             <TableCell>link</TableCell>
             <TableCell>Title</TableCell>
             <TableCell align="right">Programming language</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {assets.map((a) => (
+          {(rowsPerPage > 0
+              ? assets.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : assets
+            ).map((a) => (
             <TableRow key={a.id}>
               <TableCell>{a.id}</TableCell>
               <TableCell>{a.type}</TableCell>
               <TableCell>{a.link}</TableCell>
               <TableCell>{a.title}</TableCell>
               <TableCell align="right">{a.progLang}</TableCell>
+              <TableCell align="right">
+                <IconButton className={styles.link}>
+                  <Link to="/asset/edit" className={styles.link}>
+                    <EditIcon />
+                  </Link>
+                </IconButton>
+                <IconButton className={styles.link}>
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+        component="div"
+        count={assets.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </React.Fragment>
   );
 }
