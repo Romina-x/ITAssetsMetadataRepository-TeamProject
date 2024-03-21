3058714e7f73023799e287be616911470cadc7c0
Kyle Piazza-Nickson <zlac156@live.rhul.ac.uk>
2024-03-21 11:10:17 +0000

    Changes login to have max attempts 3

--- UI/src/components/Login.js
+++ UI/src/components/Login.js
@@ -3,33 +3,39 @@ import { TextField, Button, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 import * as LoginAPI from "../utility/LoginAPI";
+let attempts = 1;
 
 export default function Login() {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [error, setError] = React.useState("");
-  const [loading, setLoading] = React.useState(false)
+  const [loading, setLoading] = React.useState(false);
 
 
   const handleLogin = async () => {
     setLoading(true);
     setError("");
-    try {
-      const response = await LoginAPI.login({
-        username,
-        password
-      });
-      if (response.message.toLowerCase().includes("successful")) {
-        localStorage.setItem("token", response.token);
-        window.location.href = "asset/find";
-      } else {
-        setError("Invalid username or password");
-      }
-    } catch (error) {
-      setError("An error occurred, please try again later");
-    } finally {
-      setLoading(false);
-    }
+    if (attempts < 3) {
+	    try {
+	      const response = await LoginAPI.login({
+	        username,
+	        password
+	      });
+	      if (response.message.toLowerCase().includes("successful")) {
+	        localStorage.setItem("token", response.token);
+	        window.location.href = "asset/find";
+	      } 
+	    } catch (error) {
+	      console.log(error);
+	      attempts++;
+	      setError("Invalid username or password, attempts remaining: " + (4 - attempts));
+	    } finally {
+	      setLoading(false);
+	    }
+	    } else {
+	      setError("Max attempts exceeded");
+	      setLoading(false);
+		}
   };
 
   return (
