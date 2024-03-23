043acfbfacb34e36d337137be84d7173900b2e44
Richard Palissery <zlac430@live.rhul.ac.uk>
2024-03-23 21:25:12 +0000

    finished tests for Find Asset page

--- UI/cypress/e2e/FindAssetTests.cy.js
+++ UI/cypress/e2e/FindAssetTests.cy.js
@@ -12,4 +12,24 @@ describe("Find Asset Page", () => {
     cy.get("thead").contains("Author").should("exist");
     cy.get("thead").contains("Actions").should("exist");
   });
+
+it('renders table with asset data', () => {
+    cy.get('table').should('exist');
+    cy.get('tbody').find('tr').should('have.length.above', 0);
+  });
+
+  it('navigates to correct page when view icon is clicked', () => {
+    cy.get('tbody').find('tr').first().find('a').click();
+    cy.url().should('include', 'http://localhost:3000/asset/open/');
+  });
+
+  it('opens edit confirmation dialog when edit icon is clicked', () => {
+    cy.get('tbody').find('tr').first().find('button').eq(1).click();
+    cy.get('@mui/icons-material/Edit').should('be.visible');
+  });
+
+  it('opens delete confirmation dialog when delete icon is clicked', () => {
+    cy.get('tbody').find('tr').first().find('button').eq(2).click();
+    cy.get('@mui/icons-material/Delete').should('be.visible');
+  });
 });
