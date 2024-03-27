52aa088990b1552045c1dfc06972733943da0a61
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2024-03-27 23:46:49 +0000

    Packages config, controller, DTO and filter comply to checkstyle

--- src/main/java/application/controller/UserController.java
+++ src/main/java/application/controller/UserController.java
@@ -15,56 +15,77 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Controller for user related methods.
+ */
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private UserDetailsServiceImp userService;
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private UserDetailsServiceImp userService;
 
-    @GetMapping("/user/find/all")
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        List<User> users = userRepository.findAll();
-        List<UserDTO> usersDTOs = new ArrayList<>();
+  /**
+   * Finds all users from the database.
+   * @return users
+   */
+  @GetMapping("/user/find/all")
+  public ResponseEntity<List<UserDTO>> getAllUsers() {
+    List<User> users = userRepository.findAll();
+    List<UserDTO> usersDTOs = new ArrayList<>();
 
-        for (User user : users) {
-            UserDTO userDTO = new UserDTO();
-            userDTO.setID(user.getId());
-            userDTO.setFirstname(user.getFirstName());
-            userDTO.setLastname(user.getLastName());
-            userDTO.setUsername(user.getUsername());
-            userDTO.setRole(user.getRole());
-            usersDTOs.add(userDTO);
-        }
-
-        return ResponseEntity.ok(usersDTOs);
+    for (User user : users) {
+      UserDTO userDTO = new UserDTO();
+      userDTO.setID(user.getId());
+      userDTO.setFirstname(user.getFirstName());
+      userDTO.setLastname(user.getLastName());
+      userDTO.setUsername(user.getUsername());
+      userDTO.setRole(user.getRole());
+      usersDTOs.add(userDTO);
     }
 
+    return ResponseEntity.ok(usersDTOs);
+  }
 
-    @PutMapping("user/{username}/role")
-    public ResponseEntity<String> updateRole(@PathVariable String username, @RequestBody Map<String, String> requestBody) {
-        String roleValue = requestBody.get("role");
-        Role role = Role.valueOf(roleValue.toUpperCase());
-        userService.updateRole(username, role);
-        UserDetails updatedUser = userService.loadUserByUsername(username);
-        return ResponseEntity.ok(updatedUser.getAuthorities().toString());
-    }
 
-    @GetMapping("user/{username}")
-    public ResponseEntity<UserDTO> getUser(@PathVariable String username) {
-        Optional<User> optionalUser = userRepository.findByUsername(username);
-        if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-            UserDTO userDTO = new UserDTO();
-            userDTO.setID(user.getId());
-            userDTO.setFirstname(user.getFirstName());
-            userDTO.setLastname(user.getLastName());
-            userDTO.setUsername(user.getUsername());
-            userDTO.setRole(user.getRole());
-            return ResponseEntity.ok(userDTO);
-        } else {
-            return ResponseEntity.notFound().build(); // User not found
-        }
+  /**
+   * Find role of specified user from the database.
+   * 
+   * @param username of the user 
+   * @param requestBody containing role information
+   * @return role of user
+   */
+  @PutMapping("user/{username}/role")
+  public ResponseEntity<String> updateRole(@PathVariable String username,
+      @RequestBody Map<String, String> requestBody) {
+    String roleValue = requestBody.get("role");
+    Role role = Role.valueOf(roleValue.toUpperCase());
+    userService.updateRole(username, role);
+    UserDetails updatedUser = userService.loadUserByUsername(username);
+    return ResponseEntity.ok(updatedUser.getAuthorities().toString());
+  }
+
+  /**
+   * Find user by username. 
+   * 
+   * @param username of user
+   * @return user
+   */
+  @GetMapping("user/{username}")
+  public ResponseEntity<UserDTO> getUser(@PathVariable String username) {
+    Optional<User> optionalUser = userRepository.findByUsername(username);
+    if (optionalUser.isPresent()) {
+      User user = optionalUser.get();
+      UserDTO userDTO = new UserDTO();
+      userDTO.setID(user.getId());
+      userDTO.setFirstname(user.getFirstName());
+      userDTO.setLastname(user.getLastName());
+      userDTO.setUsername(user.getUsername());
+      userDTO.setRole(user.getRole());
+      return ResponseEntity.ok(userDTO);
+    } else {
+      return ResponseEntity.notFound().build(); // User not found
     }
+  }
 }
